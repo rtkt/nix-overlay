bfa2b34b45868ee77c4c7a3f5694d2d1ca4fbb09
rtkt <rtkt@protonmail.com>
2023-01-03 20:18:35 +0700

    First attempt to rewrite everything

--- flake.nix
+++ flake.nix
@@ -1,77 +1,26 @@
 {
-  description = "Flakiefied collection of Nix overlays";
-
+  description = "rtkt's overlay";
   inputs = {
-    nixpkgs = {url = "flake:nixpkgs/nixos-unstable";};
-    lib-aggregate = {url = "github:nix-community/lib-aggregate";};
+    nixpkgs.url = "flake:nixpkgs/nixos-unstable";
+    # lib-aggregate.url = "github:nix-community/lib-aggregate";};
   };
-  outputs = inputs: let
-    inherit (inputs.lib-aggregate) lib;
-    inherit (inputs) self;
 
-    ownOverlay = (
-      final: prev: let
-        ownPkgs = rec {
-          samba-for-ps2 = prev.callPackage ./samba-for-ps2;
-          # tcpflow = prev.callPackage ./tcpflow;
-        };
-      in (ownPkgs // {inherit ownPkgs;})
-    );
-  in
-    lib.flake-utils.eachSystem ["x86_64-linux"]
-    (
-      system: let
-        pkgsFor = pkgs: overlays:
-          import pkgs {
-            inherit system overlays;
-            config.allowUnfree = true;
-          };
-        pkgs_ = lib.genAttrs (builtins.attrNames inputs) (inp: pkgsFor inputs."${inp}" []);
-        opkgs_ = overlays: lib.genAttrs (builtins.attrNames inputs) (inp: pkgsFor inputs."${inp}" overlays);
-        ownpkgs = (opkgs_ [self.overlays.default]).nixpkgs;
-      in {
-        devShells.default = pkgs_.nixpkgs.mkShell {
-          nativeBuildInputs =
-            []
-            ++ (with pkgs_.nixpkgs; [
-              openssl
-              zlib
-              libpcap
-              boost
-              pkg-config
-              libxslt
-              heimdal
-              docbook_xml_dtd_45
-              docbook_xsl
-              readline
-              popt
-              dbus
-              libbsd
-              liburing
-              gnutls
-              libunwind
-              systemd
-              jansson
-              libtasn1
-              tdb
-              cmocka
-              rpcsvc-proto
-              nodejs-16_x
-              nodePackages.npm
-            ]);
-        };
-        packages = (
-          ownpkgs.ownPkgs
-          // {
-            default =
-              ownpkgs.linkFarmFromDrvs "nixpkgs-own-overlay"
-              (builtins.attValues ownpkgs.ownPkgs);
-          }
-        );
-      }
-    )
-    // {
-      overlay = ownOverlay;
-      overlays.default = ownOverlay;
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        self.overlays.default
+      ];
+      config = {allowUnfree = true;};
     };
+  in {
+    overlays.default = final: prev: rec {
+      samba-for-ps2 = final.callPackage ./pkgs/samba-for-ps2 {};
+    };
+    packages.x86_64-linux = pkgs;
+  };
 }
