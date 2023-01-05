6f3f42e30c3f8030bd9938c2f366169b11c27ba2
rtkt <rtkt@protonmail.com>
2023-01-05 12:02:44 +0700

    Added tor-browser overlay

--- flake.nix
+++ flake.nix
@@ -21,6 +21,15 @@
     overlays.default = final: prev: rec {
       samba-for-ps2 = final.callPackage ./pkgs/samba-for-ps2 {};
       tcpflow = final.callPackage ./pkgs/tcpflow {};
+
+      tor-browser-bundle-bin = prev.tor-browser-bundle-bin.overrideAttrs (finalAttrs: previousAttrs: {
+        version = "11.5.2";
+        lang = "en-US";
+        src = prev.fetchurl {
+          url = "https://tor.calyxinstitute.org/dist/torbrowser/11.5.2/tor-browser-linux64-11.5.2_en-US.tar.xz";
+          sha256 = "sha256-kM3OOFTpEU7nIyqqdGcqLZ86QLb6isM5cfWG7jo891o=";
+        };
+      });
     };
     packages.x86_64-linux = pkgs;
   };
