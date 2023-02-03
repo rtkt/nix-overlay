c50f6978bf75c6e915abcf9f3365dc4a854a0502
rtkt <rtkt@protonmail.com>
2023-02-03 22:19:45 +0700

    Added draft OPL PC Tools package

---
+++ pkgs/opl-pc-tools/default.nix
@@ -0,0 +1,22 @@
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+  autoreconfHook,
+  libsForQt5,
+}:
+stdenv.mkDerivation rec {
+  pname = "opl-pc-tools";
+  version = "3.0";
+
+  src = fetchFromGitHub {
+    owner = "brainstream";
+    repo = "OPL-PC-Tools";
+    rev = "v3.0";
+    sha256 = "0vbm097jhi5n8pg08ia1yhzc225zv9948blb76f4br739l9l22vq";
+  };
+
+  nativeBuildInputs = [libsForQt5.wrapQtAppsHook cmake autoreconfHook];
+  buildInputs = [libsForQt5.qt5.qtbase];
+}
