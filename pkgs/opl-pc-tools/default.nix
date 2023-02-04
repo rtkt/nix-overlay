1dc1f89b9accaed5aa2d5f681445ba5a5ac8140c
rtkt <rtkt@protonmail.com>
2023-02-04 11:24:11 +0700

    Set generic architecture and enabled fetching submodules

--- pkgs/opl-pc-tools/default.nix
+++ pkgs/opl-pc-tools/default.nix
@@ -5,7 +5,6 @@
   cmake,
   autoreconfHook,
   libsForQt5,
-  python2,
 }:
 stdenv.mkDerivation rec {
   pname = "opl-pc-tools";
@@ -16,8 +15,11 @@ stdenv.mkDerivation rec {
     repo = "OPL-PC-Tools";
     rev = "v3.0";
     sha256 = "0vbm097jhi5n8pg08ia1yhzc225zv9948blb76f4br739l9l22vq";
+    fetchSubmodules = true;
   };
 
   nativeBuildInputs = [libsForQt5.wrapQtAppsHook cmake autoreconfHook];
-  buildInputs = [libsForQt5.qt5.qtbase python2];
+  buildInputs = [libsForQt5.qt5.qtbase];
+
+  cmakeFlags = ["-DMARCH=x86_64"];
 }
