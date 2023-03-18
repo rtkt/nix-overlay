d6afc6e7b22058a597c44f4e0f67b192280779dc
rtkt <rtkt@protonmail.com>
2023-03-18 17:11:12 +0700

    Added overrides to the overlay

--- flake.nix
+++ flake.nix
@@ -21,26 +21,24 @@
       localPkgs = import ./default.nix {pkgs = final;};
     in {
       inherit (localPkgs) samba-for-ps2 tcpflow n8n oplpctools;
+      tor-browser-bundle-bin = prev.tor-browser-bundle-bin.overrideAttrs (finalAttrs: previousAttrs: {
+        src = prev.fetchurl {
+          url = "https://tor.calyxinstitute.org/dist/torbrowser/11.5.8/tor-browser-linux64-11.5.8_en-US.tar.xz";
+          sha256 = "sha256-/KK9oTijk5dEziAwp5966NaM2V4k1mtBjTJq88Ct7N0=";
+        };
+      });
+      google-authenticator =
+        (prev.google-authenticator.overrideAttrs (finalAttrs: previousAttrs: {
+          preConfigure = null;
+        }))
+        .override {
+          qrencode = null;
+        };
+      gnupg-minimal = prev.gnupg.override {
+        enableMinimal = true;
+      };
     };
-    # rec {
 
-    #   tor-browser-bundle-bin = prev.tor-browser-bundle-bin.overrideAttrs (finalAttrs: previousAttrs: {
-    #     src = prev.fetchurl {
-    #       url = "https://tor.calyxinstitute.org/dist/torbrowser/11.5.8/tor-browser-linux64-11.5.8_en-US.tar.xz";
-    #       sha256 = "sha256-/KK9oTijk5dEziAwp5966NaM2V4k1mtBjTJq88Ct7N0=";
-    #     };
-    #   });
-    #   google-authenticator =
-    #     (prev.google-authenticator.overrideAttrs (finalAttrs: previousAttrs: {
-    #       preConfigure = null;
-    #     }))
-    #     .override {
-    #       qrencode = null;
-    #     };
-    #   gnupg-minimal = prev.gnupg.override {
-    #     enableMinimal = true;
-    #   };
-    # };
     packages.x86_64-linux = import ./default.nix {
       pkgs = import nixpkgs {
         inherit system;
